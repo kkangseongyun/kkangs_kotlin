3fe3a273976406c08baeb56a9177aaa5258d945c
kkang <kkang104@gmail.com>
2018-01-15 15:59:02 +0900

    Add files via upload

---
+++ 소스파일/IdeaProjects/KotlinLab03MavenJVM/src/main/kotlin/com/example/Hello.kt
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.StringUtils
+/**
+ * 깡샘의 코틀린 프로그래밍 - 루비페이퍼
+ * 위의 책에 담겨져 있는 코드로 설명 및 활용 방법은 교제를 확인해 주세요.
+ */
+fun main(args: Array<String>) {
+    val data=" Hello World !! kkang "
+    println(StringUtils.deleteWhitespace(data))
+}
+
---
+++ 소스파일/IdeaProjects/KotlinLab03MavenJVM/src/test/kotlin/com/example/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.example
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
